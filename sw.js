7260fb833d429623efd3b023d9bc20bb8199e873
Koren <korenphireh22@gmail.com>
2026-07-17 23:56:27 +0300

    Design upgrade: now-strip, day dates, duration badges, live block highlight

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "campweek-v3";
+var CACHE = "campweek-v4";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
