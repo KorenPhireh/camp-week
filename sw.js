3096c4cee523fa0441c7becf1f9543b491f1c26a
Koren <korenphireh22@gmail.com>
2026-07-18 00:09:25 +0300

    Full redesign, terse coach, wake-up alarm system

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "campweek-v4";
+var CACHE = "campweek-v5";
 var ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
